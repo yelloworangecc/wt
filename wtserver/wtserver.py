7e2399ec946324c4ca4863c7352c644c29d856a0
黄大橙yellow <yelloworangecc@qq.com>
2021-12-03 11:25:19 +0800

    add feather and chart.js, add 'templates' folder

--- wtserver/wtserver.py
+++ wtserver/wtserver.py
@@ -1,13 +1,21 @@
 from flask import Flask
-# from flask import render_template
+from flask import render_template
 from flask import send_file
-# from flask import url_for
+from flask import url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return send_file('dashboard.html')
+    return render_template('dashboard.html')
+
+@app.route('/album/')
+@app.route('/album/<name>')
+def album(name=None):
+    if name is None:
+        return render_template('album.html')
+    else:
+        return send_file('album/'+name)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
