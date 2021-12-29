4ab5f75d72c23e59840d841a0fa373db6179f464
黄大橙yellow <yelloworangecc@qq.com>
2021-12-29 11:37:12 +0800

    post message to file and email

---
+++ wtserver/EmailMe.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+class EmailMe:
+    def __init__(self, domain, port):
+        self._smtp = smtplib.SMTP_SSL(domain, port)
+    def login(self, email, passcode):
+        self._email = email
+        self._smtp.login(email, passcode) 
+    def quit(self):
+        self._smtp.quit()
+    def send(self,to,who,msg):
+        content = MIMEText(who + ': ' + msg, 'plain', 'utf-8')
+        content['Subject'] = Header("WT Message", 'utf-8')
+        content['From'] = Header(self._email, 'utf-8')
+        content['To'] =  Header(to, 'utf-8')
+        to_list = []
+        to_list.append(to)
+        self._smtp.sendmail(self._email, to_list, content.as_string())
--- wtserver/wtserver.py
+++ wtserver/wtserver.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask import send_file
 from flask import url_for
+from flask import request
+from EmailMe import EmailMe
+import os,time
 
 app = Flask(__name__)
+emailMe = EmailMe('smtp.qq.com',465)
 
 @app.route('/')
 def hello_world():
@@ -21,5 +25,25 @@ def album(name=None):
 def resume():
     return render_template('resume.html')
 
+@app.route('/messageme',methods=['GET','POST'])
+def messageme():
+    ''' {'who': 'tester', 'msg': 'hello' } '''
+    if request.method == 'POST':
+        data = request.form
+        who = data['who']
+        msg = data['msg']
+        code = os.getenv('QQMAILCODE')
+        if who is None or msg is None or code is None:
+            return 'Send message FAIL'
+        else:
+            append_file_cmd = 'echo ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ', ' + who +', ' + msg + ' >> msg.txt'
+            os.system(append_file_cmd)
+            emailMe.login('1040617473@qq.com',code)
+            emailMe.send('yelloworangecc@qq.com',who,msg)
+            return 'Send message OK'
+        
+    if request.method == 'GET':
+        return render_template('msgform.html')
+
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True,host='0.0.0.0',port=8080)
